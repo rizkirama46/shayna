0aa582abbea672e18bb963b2c0f832f69ed59ebd
DESKTOP-HF6FA4A\RAMA <rizkirama46@gmail.com>
2021-02-04 10:43:14 +0700

    upload project vue

---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <HeaderShayna />
+    <HeroShayna />
+    <WomenBanner />
+    <InstaShayna />
+    <PartnerShayna />
+    <FooterShayna />
+  </div>
+</template>
+
+<script>
+import HeaderShayna from '@/components/HeaderShayna.vue';
+import HeroShayna from '@/components/HeroShayna.vue';
+import WomenBanner from '@/components/WomenBanner.vue';
+import InstaShayna from '@/components/InstaShayna.vue';
+import PartnerShayna from '@/components/PartnerShayna.vue';
+import FooterShayna from '@/components/FooterShayna.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    HeaderShayna,
+    HeroShayna,
+    WomenBanner,
+    InstaShayna,
+    PartnerShayna,
+    FooterShayna
+  }
+}
+</script>
